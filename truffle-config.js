73a9d77c2141a1844065dd43334cf3981a4c3325
jw418 <94624740+jw418@users.noreply.github.com>
2022-05-02 23:46:17 +0200

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +10,53 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777", 
+    }, 
+    
+    // mainnet: {
+    //   provider: function() {
+    //     return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`)
+    //   },
+    //   network_id: 1
+    // },
+
+    // rinkeby: {
+    //   provider: function() {
+    //     return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`)
+    //   },
+    //   network_id: 4
+    // },
+    // ropsten: {
+    //   provider: function() {
+    //     return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`)
+    //   },
+    //   network_id: 3
+    // }
+  }, 
+  
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+       version: "0.8.13",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+       settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+      //  evmVersion: "byzantium"
+      }
     }
   }
 };
