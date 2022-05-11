0f484b92d140f6e5e09b79cb1568d8bedcfc4fa8
jw418 <ju.wolff@free.fr>
2022-05-11 02:23:03 +0200

    delete WalletMultisig.sol && update tests && update 2_deploy_contracts.js

--- test/PayementSpliter.js
+++ test/PayementSpliter.js
@@ -1,3 +1,17 @@
 const PayementSpliter = artifacts.require("./PayementSpliter.sol");
-const { BN } = require('@openzeppelin/test-helpers');
+const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
+const constants = require('@openzeppelin/test-helpers/src/constants');
+
+
+contract('ContractNFT', function (accounts) {
+    const owner = accounts[0];
+    const user1 = accounts[1];
+    const user2 = accounts[2];
+
+    beforeEach(async function () {
+        this.PayementSpliterInstance = await PayementSpliter.new({ from: owner });
+        
+    });
+
+}
--- test/RatRaceNFT.js
+++ test/RatRaceNFT.js
@@ -1,3 +1,21 @@
-const ContractNFT = artifacts.require("./ContractNFT.sol");
-const { BN } = require('@openzeppelin/test-helpers');
+const RatRaceNFT = artifacts.require("./RatRaceNFT.sol");
+const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
+const constants = require('@openzeppelin/test-helpers/src/constants');
+
+contract('RatRaceNFT', function (accounts) {
+    const owner = accounts[0];
+    const user1 = accounts[1];
+    const user2 = accounts[2];
+
+    beforeEach(async function () {
+        this.RatRaceNFTInstance = await RatRaceNFT.new({ from: owner });
+        
+    });
+
+     // on vérifie bien que l'etat isRegisterd est bien sur false quand le contrat est deployé
+     it('Max_supply must be equal to 3333', async function () {
+        const maxSupply = await this.RatRaceNFTInstance.max_supply();        
+        await expect(maxSupply).to.be.bignumber.equal('3333', "Max supply is not 3333");
+    });
+})
