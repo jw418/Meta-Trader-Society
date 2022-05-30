90ba208b9d121d1edae1caf868154950b2bad0d5
jw418 <ju.wolff@free.fr>
2022-05-30 07:33:51 +0200

    update front components Team

--- client/src/components/Team.js
+++ client/src/components/Team.js
@@ -7,23 +7,27 @@ const Team = () => {
      <div className="team_members">
          <div className="team_member">
             <img className="team_member_img" src="../img/cdp_1.png"/>
-            <h3>CDP1</h3>
-            <p>bala bla bla ùdloijzjd ùdo, qzji</p>
+            <h3>CDP_1</h3>
+            <p>Chef de projet</p>
+            <p>Product Owner</p>
         </div>
         <div className="team_member">
             <img className="team_member_img" src="../img/cdp_2.png"/>
-            <h3>CDP2</h3>
-            <p>bala bla bla ùdloijzjd ùdo, qzji</p>
+            <h3>CDP_2</h3>
+            <p>Chef de projet</p>
+            <p>Scrum Master</p>
         </div>
         <div className="team_member">
             <img className="team_member_img" src="../img/dev_1.png"/>
-            <h3>DEV1</h3>
-            <p>bala bla bla ùdloijzjd ùdo, qzji</p>
+            <h3>Dev_1</h3>
+            <p>Développeur Blockchain et Front-end</p>
+            <p>En charge du site de Mint et des Smart-Contract</p>
         </div>
         <div className="team_member">
             <img className="team_member_img" src="../img/dev_2.png"/>
-            <h3>Tristan Boettger-Magnier</h3>
-            <p>Dévoloppeur depuis...</p>
+            <h3>Dev_2</h3>
+            <p>Développeur Blockchain et Front-end</p>
+            <p>En charge de la collection NFT et des Smart-Contract</p>
         </div>
      </div>       
     </div>
